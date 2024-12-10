99f99e761f2c629d3ec1004432febd366103f026
Jenny Massari <jennymartins13@gmail.com>
2024-12-09 16:23:20 -0800

    Updated the ChatEntry and App components to display a single chat message.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css';
+import messages from './data/messages.json';
+import ChatEntry from './components/ChatEntry';
 
 const App = () => {
+  const firstMessage = messages[0];
+
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
+        {
+          <ChatEntry
+            sender={firstMessage.sender}
+            body={firstMessage.body}
+            timeStamp={firstMessage.timeStamp}
+          />
+        /* Wave 01: Render one ChatEntry component
         Wave 02: Render ChatLog component */}
       </main>
     </div>
